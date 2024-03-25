9e77c03057778482bfe8814da6b0857639d1dad2
tirtahakimpambudhi <awebthp@gmail.com>
2024-03-25 04:58:34 -0400

    write file helloworld.go v.2

--- start/helloworld.go
+++ start/helloworld.go
@@ -1,1 +1,21 @@
-package start
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+const (
+	width  float32 = 64
+	height float32 = 64
+)
+
+func main() {
+	application := app.New()
+	window := application.NewWindow("hello world")
+	window.Resize(fyne.Size{Width: width, Height: height})
+
+	window.SetContent(widget.NewLabel("Hello World!!\nFirst GUI With Fyne Golang"))
+	window.ShowAndRun()
+}
