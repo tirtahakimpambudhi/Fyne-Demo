00787c10c225ed7dc09f63ea720d5e851667c5c3
tirtahakimpambudhi <awebthp@gmail.com>
2024-03-25 05:26:49 -0400

    change width, height window and adding comments

--- start/helloworld.go
+++ start/helloworld.go
@@ -6,16 +6,21 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+// constant width and height window
 const (
-	width  float32 = 64
-	height float32 = 64
+	width  float32 = 400
+	height float32 = 600
 )
 
 func main() {
+	//for instance class App
 	application := app.New()
+	//for instance class Window
 	window := application.NewWindow("hello world")
+	//Resizing width and height window
 	window.Resize(fyne.Size{Width: width, Height: height})
-
+	// Set content window with label
 	window.SetContent(widget.NewLabel("Hello World!!\nFirst GUI With Fyne Golang"))
+	// showing window and running
 	window.ShowAndRun()
 }
