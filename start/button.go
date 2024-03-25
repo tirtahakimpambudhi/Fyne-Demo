f4740b828103cc8970aab61bccf508a5a4cee5ca
tirtahakimpambudhi <awebthp@gmail.com>
2024-03-25 19:53:58 -0400

    write file in start/button.go v.1.0

--- start/button.go
+++ start/button.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"go_fyne_demo/config"
+)
+
+func main() {
+	// For instance class App
+	application := app.New()
+	// For instance class Window
+	window := application.NewWindow("Button")
+	// Resizing width and height window
+	window.Resize(fyne.NewSize(config.Width, config.Height))
+	// Set content window with label
+	window.SetContent(container.NewVBox(
+		widget.NewLabel("Try Button in Fyne Golang!!"),
+		// Set New Button
+		widget.NewButton("click!!", func() {
+			// Trigger every button clicked
+			fmt.Println("clicked")
+		}),
+	))
+	// Logging
+	fmt.Println("Running Application")
+	// Showing window
+	window.ShowAndRun()
+}
