234f19817e09c66f7728e266a1f47aa8b1266772
tirtahakimpambudhi <awebthp@gmail.com>
2024-03-25 10:09:23 -0400

    debug helloworld.go cannot show and run

--- start/helloworld.go
+++ start/helloworld.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 // constant width and height window
 const (
-	width  float32 = 400
-	height float32 = 600
+	width  = 400
+	height = 600
 )
 
 func main() {
-	//for instance class App
+	// For instance class App
 	application := app.New()
-	//for instance class Window
+	// For instance class Window
 	window := application.NewWindow("hello world")
-	//Resizing width and height window
-	window.Resize(fyne.Size{Width: width, Height: height})
+	// Resizing width and height window
+	window.Resize(fyne.NewSize(width, height))
 	// Set content window with label
-	window.SetContent(widget.NewLabel("Hello World!!\nFirst GUI With Fyne Golang"))
-	// showing window and running
+	window.SetContent(container.NewVBox(
+		widget.NewLabel("Hello World!!"),
+		widget.NewLabel("First GUI With Fyne Golang"),
+	))
+	// Logging
+	fmt.Println("Running Application")
+	// Showing window
 	window.ShowAndRun()
 }
