154fe6a93c4a9da1a14a1a61a7432ccffb629ecd
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-15 22:18:34 +0000

    Apr 16, 2024, 5:18 AM

--- start/link.go
+++ start/link.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	""
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"go_fyne_demo/config"
 )
 
 func main(){
-	
+	// instance app class to object
+	application := app.New()
+	// instance window class to object
+	//  params title string 
+	window := application.NewWindow("Hyper Link")
+	// resize window
+	// params fyne.size struct
+	window.Resize(fyne.Size{Width: config.Width, Height: config.Height})
 }
